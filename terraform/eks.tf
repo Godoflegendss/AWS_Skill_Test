3cfc58a4a3b378767696df62df230862e2158106
Karthi <karthi001bus@hotmail.com>
2025-09-24 20:25:02 +0530

    workflow

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -98,17 +98,17 @@ data "aws_eks_cluster_auth" "cluster" {
   
 }
 
+# Kubernetes provider
 provider "kubernetes" {
-    host = aws_ceks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token = data.aws_eks_cluster_auth.cluster.token
-  
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
-  kubernetes{
-    host = aws_eks_cluster.this.endpoint
+  kubernetes {
+    host                   = aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token = data.aws_eks_cluster_auth.cluster.token
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
