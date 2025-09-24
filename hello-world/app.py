827fd3e89515c5fd4c96da2ad85addc0485e8e4a
Karthi <karthi001bus@hotmail.com>
2025-09-24 19:58:28 +0530

    Message change

--- hello-world/app.py
+++ hello-world/app.py
@@ -22,7 +22,7 @@ Value=get_smm("skill-test")
 
 @app.route('/')
 def hello():
-    return jsonify({"message":Value})
+    return jsonify({"Result":Value})
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8000)
