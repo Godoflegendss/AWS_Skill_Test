4b621c18ddf34ca83a61c39b3adf8c4bf15b89a6
Karthi <karthi001bus@hotmail.com>
2025-09-24 20:51:57 +0530

    Workfliw

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,29 +1,27 @@
 import boto3
-from flask import jsonify,Flask
+from flask import Flask, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-def get_smm(Param_name):
-    ssm_client=boto3.client("ssm")
-    print(Param_name)
-    response=ssm_client.get_parameter(
-        
-        Name=Param_name
-        
 
+def get_smm(param_name):
+    ssm_client = boto3.client("ssm")
+    print(param_name)
+    response = ssm_client.get_parameter(
+        Name=param_name
     )
-
-    parameter_value=response['Parameter']['Value']
-
+    parameter_value = response["Parameter"]["Value"]
     print(parameter_value)
     return parameter_value
 
-Value=get_smm("skill-test")
 
-@app.route('/')
+value = get_smm("skill-test")
+
+
+@app.route("/")
 def hello():
-    return jsonify({"Result":Value})
+    return jsonify({"Result": value})
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
