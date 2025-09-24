1a015fb13009852a0caa9bce5b9e2ce87506b791
Karthi <karthi001bus@hotmail.com>
2025-09-24 22:04:49 +0530

    Workflow

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -75,7 +75,7 @@ resource "aws_route_table_association" "public_association" {
 
 resource "aws_route_table" "private" {
     count=length(aws_subnet.private)
-    vpc_id = aws_vpc.main
+    vpc_id = aws_vpc.main.id
     route {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat[count.index].id
